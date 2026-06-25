e12bc5988a3325302b8ea2bb05df9ae266a92f2d
RaresSelaru <109241561+RaresSelaru@users.noreply.github.com>
2026-06-25 20:08:08 +0300

    Restore sales management quiz pack

--- data/grile.js
+++ data/grile.js
@@ -9088,5 +9088,365 @@ window.QUIZ_PACKS = [
         "answerIndex": 1
       }
     ]
+  },
+  {
+    "id": "vanzari-examen",
+    "title": "Managementul Vânzărilor",
+    "subject": "Managementul Vânzărilor",
+    "source": "grile vanzari (4) (1).docx",
+    "questions": [
+      {
+        "id": 1,
+        "text": "Devizul reprezintă:",
+        "options": [
+          "O estimare cifrică precisă a prestaţiilor propuse clientului",
+          "Dovada creanţei furnizorului",
+          "Dovada datoriei clientului",
+          "Acordul cu clientul asupra unei cantităţi cumpărate la un preţ dat şi în funcţie de condiţiile comerciale acceptate de cele două părţi"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 2,
+        "text": "Bonul de comandă:",
+        "options": [
+          "este transmis imediat serviciului contabil pentru facturare, apoi întreprinderii pentru executarea ordinului clientului",
+          "este transmis imediat întreprinderii pentru executarea ordinului clientului, apoi serviciului contabil pentru facturare",
+          "este întocmit frecvent, atunci când contractul necesită o descriere a operaţiilor de efectuat de către furnizor",
+          "este întocmit frecvent atunci când clientul nu poate vedea produsul pe care-l cumpără în momentul efectuării tranzacţiei"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 3,
+        "text": "Remizele sunt:",
+        "options": [
+          "destinate fidelizării clientului",
+          "reduceri acordate la sfârşitul perioadei (an, trimestru) asupra cifrei de afaceri nete fără taxe realizată în cursul unei perioade",
+          "reduceri acordate pentru a ţine seama de un defect, pentru o întârziere a livrării, pentru mărfuri deteriorate, pentru sfârşit de sezon etc.",
+          "reduceri acordate pentru cantităţile vândute, pentru calitatea clientului (client foarte important, client fidel, client profesionist) sau în cadrul unei operaţiuni profesionale"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 4,
+        "text": "Valoarea adăugată se calculează astfel:",
+        "options": [
+          "Valoarea bunurilor şi serviciilor cumpărate de întreprindere – Valoarea bunurilor şi serviciilor vândute de întreprindere",
+          "Valoarea bunurilor şi serviciilor vândute de întreprindere",
+          "Valoarea bunurilor şi serviciilor cumpărate de întreprindere",
+          "Valoarea bunurilor şi serviciilor vândute de întreprindere - Valoarea bunurilor şi serviciilor cumpărate de întreprindere"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 5,
+        "text": "Indicele de marcă se determină prin următoarea formulă:",
+        "options": [
+          "Preţul de vânzare / Marja comercială",
+          "Marja comercială / Preţul de vânzare",
+          "Marja comercială / Costul de cumpărare",
+          "Costul de cumpărare / Marja comercială"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 6,
+        "text": "Indicele de marjă se determină prin următoarea formulă:",
+        "options": [
+          "Marja comercială / Preţul de vânzare",
+          "Costul de cumpărare / Marja comercială",
+          "Marja comercială / Costul de cumpărare",
+          "Preţul de vânzare / Marja comercială"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 7,
+        "text": "contul client conţine:",
+        "options": [
+          "istoricul relaţiilor comerciale care rezultă din notarea fiecărei vizite şi a rezultatului obţinut în urma acesteia",
+          "vânzările care conduc la întocmirea de facturi înregistrate în debitul contului",
+          "frecvenţa şi natura cumpărărilor",
+          "articolele cele mai comandate"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 8,
+        "text": "Informaţiile generale grupate în contul client sunt:",
+        "options": [
+          "informaţii generale asupra întreprinderii",
+          "istoricul relaţiilor comerciale care rezultă din notarea fiecărei vizite şi a rezultatului obţinut în urma acesteia",
+          "cereri specifice formulate de client faţă de vânzător",
+          "nume şi coordonate ale interlocutorului"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 9,
+        "text": "Clasificarea clienţilor în 3 categorii (A, B şi C) corespunde:",
+        "options": [
+          "analizei 20 / 80",
+          "analizei ABC",
+          "analizei 80 / 20",
+          "analizei ACB"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 10,
+        "text": "Conform analizei 20 / 80, „marii clienţi” sunt:",
+        "options": [
+          "sunt grupaţi în categoria 80%",
+          "realizează minimul cifrei de afaceri",
+          "sunt grupaţi în categoria 20%",
+          "sunt grupaţi în categoria 70%"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 11,
+        "text": "Cheltuielile variabile:",
+        "options": [
+          "sunt independente de volumul sau nivelul activităţii întreprinderii",
+          "sunt cheltuielile legate de dobânzile plătite la credite lunare",
+          "sunt amortismentele practicate asupra imobilizărilor",
+          "sunt dependente de volumul sau nivelul activităţii întreprinderii"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 12,
+        "text": "Marja asupra costului variabil se calculează astfel:",
+        "options": [
+          "cost variabil – cifra de afaceri",
+          "cifra de afaceri – cheltuieli fixe",
+          "cheltuieli fixe – cifra de afaceri",
+          "cifra de afaceri – cost variabil"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 13,
+        "text": "La nivelul pragului de rentabilitate:",
+        "options": [
+          "marja asupra costului variabil = costurile fixe",
+          "marja asupra costului variabil > costurile fixe",
+          "costurile fixe > marja asupra costului variabil",
+          "costurile fixe < marja asupra costului variabil"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 14,
+        "text": "Marja asupra costului variabil se calculează astfel:",
+        "options": [
+          "cost variabil – cifra de afaceri",
+          "cifra de afaceri – cheltuieli fixe",
+          "cheltuieli fixe – cifra de afaceri",
+          "cifra de afaceri – cost variabil"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 15,
+        "text": "Pragul de rentabilitate reprezintă cifra de afaceri astfel încât:",
+        "options": [
+          "Rezultatul > 0",
+          "Rezultatul < 0",
+          "Rezultatul = 0",
+          "Rezultatul ≥ 0"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 16,
+        "text": "Printre avantajele forţei de vânzare delegată supletivă enumerăm:",
+        "options": [
+          "controlul activităţii vânzătorilor",
+          "control dificil privind activitatea vânzătorilor",
+          "mai bună integrare şi dezvoltare a spiritului de echipă, a culturii de întreprindere",
+          "poate acţiona pe perioade scurte de timp"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 17,
+        "text": "Printre inconvenientele forţei de vânzare delegată permanent enumerăm:",
+        "options": [
+          "gestionarea unui personal salariat",
+          "suportarea de către întreprindere a tuturor cheltuielilor legate de forţa de vânzare, chiar dacă cifra de afaceri este scăzută",
+          "control redus asupra activităţii vânzătorilor",
+          "devine imediat operaţională"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 18,
+        "text": "Dintre avantajele pentru vânzător a salariului de drept comun enumerăm:",
+        "options": [
+          "posibilitatea de a orienta activitatea spre obiective precise, atât cantitativ, cât şi calitativ",
+          "siguranţă, acoperire socială",
+          "legătură de subordonare juridică",
+          "gestiunea unui personal salariat"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 19,
+        "text": "Dintre inconvenientele pentru întreprindere a salariului de drept comun, enumerăm:",
+        "options": [
+          "legătură de subordonare juridică",
+          "cheltuieli fixe obligatoriu de acoperit",
+          "autonomie redusă în prestarea muncii",
+          "nu deţine nici un drept asupra clientelei pe care a creat-o şi a dezvoltat-o"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 20,
+        "text": "Dintre avantajele unei reţele directe enumerăm:",
+        "options": [
+          "costuri de structură reduse",
+          "o mai bună supleţe a reţelei",
+          "diversificare mai uşoară",
+          "mai buna acoperire a pieţei"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 21,
+        "text": "Dintre inconvenientele unei reţele indirecte, enumerăm:",
+        "options": [
+          "costuri de structură ridicate",
+          "reţeaua nu este întotdeauna adaptată atunci când apar produse noi sau pieţe noi",
+          "nevoia de fidelizare",
+          "costuri de structură reduse"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 22,
+        "text": "Dintre avantajele pentru întreprindere a candidaturilor interne, enumerăm:",
+        "options": [
+          "valorizarea colaboratorilor întreprinderii",
+          "o alegere mai largă şi mai bună dintr-un număr mai mare de candidaţi",
+          "reînnoirea potenţialului uman al întreprinderii",
+          "mai buna cunoaştere a candidaţilor"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 23,
+        "text": "Dintre dezavantajele pentru întreprindere a candidaturilor externe, menţionăm:",
+        "options": [
+          "riscul creării unor „prietenii”",
+          "dificultăţi privind abandonarea unor obişnuinţe căpătate în întreprindere pe vechiul post",
+          "cost ridicat al procedurii de recrutare",
+          "gelozii suscitate de promovarea internă"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 24,
+        "text": "Dintre avantajele micilor anunţuri, amintim:",
+        "options": [
+          "cost ridicat",
+          "contactarea unui număr mare de candidaţi într-un interval de timp limitat",
+          "durata procedurii (rezervarea de spaţiu, creaţia anunţului, organizarea urmăririi efectelor anunţului)",
+          "risc de banalizare a comunicării"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 25,
+        "text": "Scrisoarea de candidatură:",
+        "options": [
+          "este dactilografiată",
+          "este adesea manuscrisă",
+          "conţine teste de evaluare a cunoştinţelor, atitudinii, personalităţii, care permit alcătuirea aşa-numitului „inventar al personalităţii vânzătorului”",
+          "se prezintă sub forma unui chestionar"
+        ],
+        "answerIndex": 1
+      },
+      {
+        "id": 26,
+        "text": "Baza de calcul a comisionului poate fi:",
+        "options": [
+          "cifra de afaceri",
+          "costul",
+          "profitul",
+          "preţul"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 27,
+        "text": "Indicii de comision trebuie să ţină seama de:",
+        "options": [
+          "cifră de afaceri",
+          "profit",
+          "concurenţă",
+          "costuri"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 28,
+        "text": "Cauzele care duc la eşecuri pot fi identificate printr-o analiză atentă:",
+        "options": [
+          "a reclamaţiilor",
+          "indicatorilor de performanţă a vânzătorilor",
+          "comenzilor primite",
+          "afacerilor încheiate"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 29,
+        "text": "Avantajele formării de către organismele specializate sunt:",
+        "options": [
+          "o bună cunoaştere a produselor",
+          "cost mai redus",
+          "o largă posibilitate de alegere a studiilor adaptate la nevoi",
+          "o bună cunoaştere a clientelei"
+        ],
+        "answerIndex": 2
+      },
+      {
+        "id": 30,
+        "text": "Limitele formării inter-întreprindere sunt:",
+        "options": [
+          "risc de divulgare a strategiei comerciale către întreprinderile concurente",
+          "cost ridicat când întreprinderea nu foloseşte mulţi vânzători",
+          "absenţa schimbului de idei şi experienţă cu comercialii din alte întreprinderi",
+          "cost mai redus"
+        ],
+        "answerIndex": 0
+      },
+      {
+        "id": 31,
+        "text": "Pentru măsurarea rezultatelor calitative, criteriile de evaluare sunt:",
+        "options": [
+          "evoluţia cifrei de afaceri",
+          "media pe o comandă",
+          "media pe vizită",
+          "indicele de satisfacţie a clienţilor"
+        ],
+        "answerIndex": 3
+      },
+      {
+        "id": 32,
+        "text": "Pentru măsurarea rezultatelor cantitative, criteriile de evaluare sunt:",
+        "options": [
+          "indicele de retururi",
+          "număr mediu de vizite pe o comandă",
+          "calitatea contactelor cu clientela",
+          "integrarea noilor veniţi"
+        ],
+        "answerIndex": 1
+      }
+    ]
   }
 ];
